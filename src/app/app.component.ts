f26ad9944dfd14897ff26701588715b08d0f9699
hipnick <gokhanbodur@gmail.com>
2017-05-08 17:11:05 +0200

    changes in the main screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,40 @@
-import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
+﻿import { Component, HostListener, ElementRef, ViewChild, OnInit } from '@angular/core';
+//declare var require: any;
+//const Parallax = require('parallax-js');
+declare var Parallax: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  private app: any = {
-    name: 'People',
-    description: 'People is a browser game inspired by Civilization, Imperialism, Paradox games and many others.'
-  };
+export class AppComponent implements OnInit {
+    private app: any = {
+        name: 'People',
+        description: 'People is a browser game inspired by Civilization, Imperialism, Paradox games and many other strategy games.'
+    };
 
-  @ViewChild('main') main: ElementRef;
+    private counter = 0;
+    private speed = 20;
 
-  @HostListener('mousemove', ['$event'])
-  onMousemove(event: MouseEvent) {
-    /*event.clientX
-      event.clientY */
-    /* this.main.nativeElement.style.marginRight = (event.clientX / 100) + 'px';
-     this.main.nativeElement.style.marginTop = (event.clientY / 100) + 'px';*/
-  }
+    private cities = [];
+
+    ngOnInit() {
+        var scene = document.getElementById('scene');
+
+        var parallax = new Parallax(scene);
+        setInterval(() => {
+            this.tick();
+        }, this.speed);
+    }
+
+    tick() {
+
+        if (this.counter % this.speed == 0) {
+            var angle = Math.random() * Math.PI * 2;
+            var radius = (Math.random() - 0.5) * 50;
+            this.cities.push({ x: 50 + Math.cos(angle) * radius + 'vw', y: 50 + Math.sin(angle) * radius + 'vh' });
+        }
+        this.counter++;
+    }
 }
